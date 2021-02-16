76f275c5a439a4bfb194557415e5febdb03ffce3
未来 <7814436+oldRust@user.noreply.gitee.com>
2021-02-16 21:37:43 +0800

    11139991

--- src/store/fenzhi.js
+++ src/store/fenzhi.js
@@ -6,7 +6,7 @@ export default {
     // 命名空间
     namespaced:true,
      state:{
-         item:{name:'刘子岳可真帅',key:1,ios:"17611035880",a:"硕大的"},
+         item:{name:'刘子岳可真帅不一样歌剧院',key:1,ios:"17611035880",a:"硕大的"},
          list:{}
      },
      getters:{
